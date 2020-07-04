ff22bcf5457b52dc0620b6c45dfb4769f0198043
unknown <clkennedy008@gmail.com>
2020-07-03 23:10:39 -0500

    CharacterController Camera Lerp on character

---
+++ RoboGame/Assets/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    public float lerpSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = Player.transform.position;
+        Vector3 posCam = this.gameObject.transform.position;
+        this.gameObject.transform.position = new Vector3(Mathf.Lerp(posCam.x, pos.x, Time.deltaTime * lerpSpeed), Mathf.Lerp(posCam.y, pos.y, Time.deltaTime * lerpSpeed), posCam.z);
+    }
+}
--- RoboGame/Assets/CharacterController.cs
+++ RoboGame/Assets/CharacterController.cs
@@ -1,19 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Input;
 
 public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animator;
+    public Rigidbody2D RG;
+    public SpriteRenderer spriteRenderer;
+
+    public float moveSpeed = 1f;
+    public bool moving = false;
+    public bool movePressed = false;
+    public Vector2 direction;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame    void Update()
+    void FixedUpdate(){
+
+        Move();
+        
+    }
+
+    public void Move()
     {
-        if()
+        movePressed = false;
+        
+        if(Input.GetKey(KeyCode.W)){
+            direction.y = 1;
+            animator.SetFloat("Vert", direction.y);
+            movePressed = true;
+            if(!moving){
+                moving = true;
+                animator.SetBool("Moving", moving);
+            }
+        }else if(Input.GetKey(KeyCode.S)){
+            direction.y = -1;
+            animator.SetFloat("Vert", direction.y);
+            movePressed = true;
+            if(!moving){
+                moving = true;
+                animator.SetBool("Moving", moving);
+            }
+        }else{
+            if(direction.x != 0){
+                direction.y = 0;
+                animator.SetFloat("Vert", direction.y);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.D)){
+            direction.x = 1;
+            animator.SetFloat("Horz", direction.x);
+            movePressed = true;
+            //spriteRenderer.flipX = false;
+            if(!moving){
+                moving = true;
+                animator.SetBool("Moving", moving);
+            }
+        }else if(Input.GetKey(KeyCode.A)){
+            direction.x = -1;
+            animator.SetFloat("Horz", direction.x);
+            movePressed = true;
+            //spriteRenderer.flipX = true;
+            if(!moving){
+                moving = true;
+                animator.SetBool("Moving", moving);
+            }
+        }else{
+            if(direction.y != 0){
+                direction.x = 0;
+                animator.SetFloat("Horz", direction.x);
+            }
+        }
+
+        if(!movePressed && moving){
+            moving = false;
+            animator.SetBool("Moving", moving);
+        }
+        Vector2 vel = new Vector2(0,0);
+        if(movePressed){
+            vel = direction;
+            if(vel.x != 0 && vel.y != 0){
+                vel.x *= .75f;
+                vel.y *= .75f;
+            }
+        }
+
+        RG.MovePosition(new Vector2(this.transform.position.x, this.transform.position.y) + vel * moveSpeed * Time.deltaTime);
     }
 }
